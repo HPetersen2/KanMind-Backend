1fe080aa305b5f0a003462c8521dfee00bb30e40
Henrik Petersen <149525216+HPetersen2@users.noreply.github.com>
2025-06-30 21:55:57 +0200

    change user_auth_app

--- user_auth_app/models.py
+++ user_auth_app/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class UserProfile(models.Model):
-    user_id = models.CharField(max_length=255)
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
-    email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=255)
-
+# Create your models here.
