6bc100bfaf57dff689753d0374420d93c85c3380
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-08-03 17:20:20 +0200

    add permisson for tasks

--- kanban_app/api/permissions.py
+++ kanban_app/api/permissions.py
@@ -5,6 +5,10 @@ class IsOwnerOrMember(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.owner or request.user in obj.members.all()
     
+class IsTaskAssigneeOrReviewer(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.assignee or request.user in obj.reviewer.all()
+    
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.owner
