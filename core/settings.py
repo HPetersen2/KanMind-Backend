43e5d5d3adc6835ff098a719f8809cdaf9edbe4c
HPetersen2 <petermann2@web.de>
2025-08-05 11:44:53 +0200

    add django filters

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
     'kanban_app',
     'auth_app',
 ]
@@ -131,5 +132,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ]
 }
