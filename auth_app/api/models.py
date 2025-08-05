ed0b6632ff941c00eb9fc3436bfb22f3138e9b50
Henrik Petersen <149525216+HPetersen2@users.noreply.github.com>
2025-08-05 21:36:10 +0200

    bug user profile model

--- auth_app/api/models.py
+++ auth_app/api/models.py
@@ -5,8 +5,8 @@ class UserProfile(models.Model):
     """This field creates a one-to-one relationship with the built-in Django User model.
     It ensures that each UserProfile is uniquely linked to one User instance.
     The 'on_delete=models.CASCADE' argument specifies that when the associated User is deleted,
-    the UserProfile will be deleted as well, maintaining database integrity.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)"""
+    the UserProfile will be deleted as well, maintaining database integrity."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     """This field stores the full name of the user as a character string.
     The maximum length is limited to 255 characters to accommodate long names."""
